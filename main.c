0fe467bb2b172d83c1c93c8b3884299078539148
baiching <bachmarma@gmail.com>
2025-09-07 22:39:47 +0200

    added "prepared statement" enums. Also added a wrapper for MetaCommandResult

--- main.c
+++ main.c
@@ -57,6 +57,37 @@ void close_input_buffer(InputBuffer* input_buffer) {
     free(input_buffer);
 }
 
+/**
+ * this is for any command that starts with a dot
+ * we call it "Meta command"
+ */
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+MetaCommandResult* do_meta_command(InputBuffer* input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+/**
+ * It is prepared statement which will contain CRUD eventually
+ */
+typedef enum {
+    STATEMENT_SELECT,
+    STATEMENT_INSERT
+} StatementType;
+
 int main(int argc, char* argsv[]) {
     InputBuffer* input_buffer = new_input_buffer();
 
@@ -70,6 +101,27 @@ int main(int argc, char* argsv[]) {
         }
         else {
             printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+
+            if (input_buffer->buffer[0] == '.') {
+                switch (do_meta_command(input_buffer)) {
+                    case (META_COMMAND_SUCCESS):
+                        continue;
+                    case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                        printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+                        continue;
+                }
+            }
+            Statement statement;
+            switch (prepare_statement(input_buffer, &statement)) {
+                case (PREPARE_SUCCESS):
+                    break;
+                case (PREPARE_UNRECOGNIZED_STATEMENT):
+                    printf("Unrecognized keyword at the start of '%s'.\n", input_buffer->buffer);
+                    continue;
+            }
+
+            execute_statement(&statement);
+            printf("Executed.\n");
         }
     }
 
