ad8c4d1dc7bfdf3097b7a5edcb5e53200e024e66
baiching <bachmarma@gmail.com>
2025-09-19 12:24:45 +0200

    a function that returns the memory address of a target row, which can be used to read or write in that address

--- main.c
+++ main.c
@@ -157,6 +157,26 @@ typedef struct {
 } Table;
 
 /**
+ * This way we can read/write in memory for a particular row
+ * @param table address of the table where the row is located
+ * @param row_num the number in that table
+ * @return a pointer that points to the exact location that row is stored
+ */
+void* row_slot(Table* table, uint32_t row_num) {
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if (page == NULL) {
+        // Allocate memory only when we try to access page
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+
+    return page + byte_offset;
+}
+
+
+/**
  *
  * This is mini "sql compiler"
  * @param input_buffer
