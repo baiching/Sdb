45be371c8858b971c7a662e040d1851196bae556
baiching <bachmarma@gmail.com>
2025-09-25 21:02:23 +0200

    add functions to create and free the tables space once dropped

--- main.c
+++ main.c
@@ -260,6 +260,30 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
     }
 }
 
+/**
+ * Initialize the table
+ * @param argc
+ * @param argsv
+ * @return
+ */
+Table* new_table() {
+    Table* table = (Table*) malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+
+    return table;
+}
+
+void free_table(Table* table) {
+    for (int i = 0; table->pages[i]; i++) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
+
 int main(int argc, char* argsv[]) {
     InputBuffer* input_buffer = new_input_buffer();
 
