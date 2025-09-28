705afb9e6a7e91614daefb0505b4dfaf8455ef47
baiching <bachmarma@gmail.com>
2025-09-28 20:56:19 +0200

    adding test folder and setting up CMake. I'll be using catch2 to write test cases

--- main.c
+++ main.c
@@ -59,24 +59,6 @@ void close_input_buffer(InputBuffer* input_buffer) {
 }
 
 /**
- * this is for any command that starts with a dot
- * we call it "Meta command"
- */
-typedef enum {
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
-
-MetaCommandResult do_meta_command(InputBuffer* input_buffer) {
-    if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        exit(EXIT_SUCCESS);
-    }
-    else {
-        return META_COMMAND_UNRECOGNIZED_COMMAND;
-    }
-}
-
-/**
  * It is prepared statement which will contain CRUD eventually
  */
 typedef enum {
@@ -161,6 +143,47 @@ typedef struct {
 } Table;
 
 /**
+ * Initialize the table
+ * @return new table
+ */
+Table* new_table() {
+    Table* table = (Table*) malloc(sizeof(Table));
+    table->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table->pages[i] = NULL;
+    }
+
+    return table;
+}
+
+void free_table(Table* table) {
+    for (int i = 0; table->pages[i]; i++) {
+        free(table->pages[i]);
+    }
+    free(table);
+}
+
+/**
+ * this is for any command that starts with a dot
+ * we call it "Meta command"
+ */
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table *table) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        close_input_buffer(input_buffer);
+        free_table(table);
+        exit(EXIT_SUCCESS);
+    }
+    else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+/**
  * This way we can read/write in memory for a particular row
  * @param table address of the table where the row is located
  * @param row_num the number in that table
@@ -191,7 +214,7 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
         statement->type = STATEMENT_INSERT;
         int args_input = sscanf(
-            input_buffer->buffer, "inser %d %s %s", &(statement->row_to_insert.id),
+            input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
             statement->row_to_insert.username, statement->row_to_insert.email
             );
         if (args_input < 3) {
@@ -260,66 +283,44 @@ ExecuteResult execute_statement(Statement* statement, Table* table) {
     }
 }
 
-/**
- * Initialize the table
- * @param argc
- * @param argsv
- * @return
- */
-Table* new_table() {
-    Table* table = (Table*) malloc(sizeof(Table));
-    table->num_rows = 0;
-    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
-        table->pages[i] = NULL;
-    }
-
-    return table;
-}
-
-void free_table(Table* table) {
-    for (int i = 0; table->pages[i]; i++) {
-        free(table->pages[i]);
-    }
-    free(table);
-}
-
 
 int main(int argc, char* argsv[]) {
+    Table *table = new_table();
     InputBuffer* input_buffer = new_input_buffer();
 
     while (true) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
-        else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-
-            if (input_buffer->buffer[0] == '.') {
-                switch (do_meta_command(input_buffer)) {
+        if (input_buffer->buffer[0] == '.') {
+                switch (do_meta_command(input_buffer, table)) {
                     case (META_COMMAND_SUCCESS):
                         continue;
                     case (META_COMMAND_UNRECOGNIZED_COMMAND):
                         printf("Unrecognized command '%s'.\n", input_buffer->buffer);
                         continue;
                 }
-            }
-            Statement statement;
-            switch (prepare_statement(input_buffer, &statement)) {
-                case (PREPARE_SUCCESS):
-                    break;
-                case (PREPARE_UNRECOGNIZED_STATEMENT):
-                    printf("Unrecognized keyword at the start of '%s'.\n", input_buffer->buffer);
-                    continue;
-            }
+        }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_SYNTAX_ERROR):
+                printf("Syntax error could not parse the syntax\n");
+                continue;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at the start of '%s'.\n", input_buffer->buffer);
+                continue;
+        }
 
-            execute_statement(&statement);
-            printf("Executed.\n");
+        switch (execute_statement(&statement, table)) {
+            case (EXECUTE_SUCCESS):
+                printf("Executed.\n");
+                break;
+            case (EXECUTE_TABLE_FULL):
+                printf("Error: Table full.\n");
+                break;
         }
     }
-
-    return 0;
 }
+
---
+++ test/test_db.cpp
@@ -0,0 +1,3 @@
+//
+// Created by bachm on 9/28/2025.
+//
